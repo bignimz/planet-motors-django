a0b60bd85ef8b5ea7e2187d05fa7e112951d7a62
Nimrod Chitayi <nimrod.chitayi@gmail.com>
2022-07-28 21:33:14 +0300

    added functionality to display all cars on the cars page as well as added pagination

--- cars/views.py
+++ cars/views.py
@@ -1,6 +1,31 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
+from cars.models import Car
+from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
 
 # Create your views here.
 
 def cars(request):
-    return render(request, 'cars/cars.html')
+    cars = Car.objects.all().order_by('-created_date')
+    paginator = Paginator(cars, 4)
+    page = request.GET.get('page')
+    paged_cars = paginator.get_page(page)
+
+    context = {
+        'cars': paged_cars,
+    }
+    return render(request, 'cars/cars.html', context)
+
+def car_detail(request, id):
+    single_car = get_object_or_404(Car, pk=id)
+    # car_features = Car.objects.filter('features', pk=id)
+
+    context = {
+        'single_car': single_car,
+        # 'car_features': car_features,
+    }
+    return render(request, 'cars/car_detail.html', context)
+
+def search(request):
+    cars = Car.objects.order_by('-created_date')
+    context = {'cars': cars}
+    return render(request, 'cars/search.html', context)
