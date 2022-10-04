e3ebc09aded89d691ac7a0e5ae71ffa1c7272a5f
Nimrod Chitayi <nimrod.chitayi@gmail.com>
2022-10-04 23:23:01 +0300

    configured whitenoise middleware

--- planetmotors/settings.py
+++ planetmotors/settings.py
@@ -174,7 +174,7 @@ SITE_ID = 2
 EMAIL_HOST = 'smtp-gmail.com',
 EMAIL_PORT = 587,
 EMAIL_HOST_USER = 'micmojainc@gmail.com',
-EMAIL_HOST_PASSWORD = 'Mombasa@001',
+EMAIL_HOST_PASSWORD = '',
 EMAIL_USE_TLS = True
 
 
